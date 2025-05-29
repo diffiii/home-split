e9ef5c0de375cebc856058dda72e44930da68819
diffiii <janwieslaw.marek@gmail.com>
2025-05-29 22:11:43 +0200

    backend: Fix password hashing error

--- backend/api/serializers/user.py
+++ backend/api/serializers/user.py
@@ -15,3 +15,19 @@ class UserSerializer(serializers.ModelSerializer):
     def validate_password(self, value):
         validate_password(value)
         return value
+
+    def create(self, validated_data):
+        password = validated_data.pop('password')
+        user = User.objects.create_user(**validated_data)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def update(self, instance, validated_data):
+        password = validated_data.pop('password', None)
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        if password:
+            instance.set_password(password)
+        instance.save()
+        return instance
